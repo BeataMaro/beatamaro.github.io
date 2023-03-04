6408b3e651139a46faaef5fd07811aebcb238bdd
BeataMaro <beatamaro@interia.pl>
2023-03-04 19:28:36 +0100

    refactor: config assets

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,10 @@ module.exports = {
   mode: 'development',
   entry: './src/js/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    assetModuleFilename: 'assets/[hash][ext][query]',
   },
   devServer: {
     static: path.resolve(__dirname, 'dist'),
@@ -27,13 +29,17 @@ module.exports = {
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-          },
-        ],
+        test: /\.jpg/,
+        type: 'asset/resource',
       },
+      // {
+      //   test: /\.(png|jpe?g|gif)$/i,
+      //   use: [
+      //     {
+      //       loader: 'file-loader',
+      //     },
+      //   ],
+      // },
       {
         test: /\.html$/i,
         loader: 'html-loader',
